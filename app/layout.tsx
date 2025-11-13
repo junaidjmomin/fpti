9843670a58b8dee58e832e8405ca86f65ac65de9
junaidjmomin <121440706+junaidjmomin@users.noreply.github.com>
2025-11-14 00:34:15 +0530

    Update metadata title in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+    title: '10553, 10792, 10681',
   icons: {
     icon: [
       {
